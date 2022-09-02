831a2211fe46d082560fa38f47dedf70ed8c8c7f
AnupJP <anupp@thirdrocktechkno.com>
2022-09-02 16:48:13 +0530

    Add some static markdowns

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-netlify-cms", 
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`,//`${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
